bf73fd0d7991debd82e5054f8ccf3390b685690c
biswajitdixit <buntyoec1998@gmail.com>
2021-01-21 18:46:22 +0530

    ADDED:The roll die

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,6 +2,7 @@ public class SnakeLadder{
  static final int initialPosition = 0;
  public static void main(String[]args){
   System.out.println("Welcome to snakeLadder simulation");
-  System.out.println("snake&ladder start with single player at start pos : " + initialPosition);
+  int rollDice =(int) Math.floor(Math.random()*6+1);
+  System.out.println("player roll a dice: " + rollDice);
  }
 }
