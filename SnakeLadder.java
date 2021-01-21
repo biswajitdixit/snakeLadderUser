5c5ee5bbdd3d19c341dddb45ff3f6143a3c596f0
biswajitdixit <buntyoec1998@gmail.com>
2021-01-21 18:42:18 +0530

    ADDED:the initial point added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,5 +2,6 @@ public class SnakeLadder{
  static final int initialPosition = 0;
  public static void main(String[]args){
   System.out.println("Welcome to snakeLadder simulation");
+  System.out.println("snake&ladder start with single player at start pos : " + initialPosition);
  }
 }
